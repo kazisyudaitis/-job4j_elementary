48fd083726f9df10a65fb1aab3b1b07cc7974383
Казис <yuka95@inbox.ru>
2021-12-09 22:42:22 +1000

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Yudaitis Kazis");
+        System.out.print("09.12.2021");
+    }
+}
